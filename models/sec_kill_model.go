459985d37c95b3683be5805e7c9ed52caf838d20
NaseemLi <naseem0922@proton.me>
2025-05-09 15:55:45 +0800

    秒杀表

---
+++ models/sec_kill_model.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type SecKillModel struct {
+	Model
+	GoodsID    uint       `json:"goodsID"`                     //商品ID
+	GoodsModel GoodsModel `gorm:"foreignKey:GoodsID" json:"-"` //商品模型
+	KillPrice  int        `json:"killPrice"`                   //秒杀价格
+	StartTime  time.Time  `json:"startTime"`                   //秒杀开始时间
+	EndTime    time.Time  `json:"endTime"`                     //秒杀结束时间
+}
