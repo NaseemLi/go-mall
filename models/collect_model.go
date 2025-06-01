6512cb920d56d47057c300d41924cae536547300
NaseemLi <naseem0922@proton.me>
2025-06-01 21:30:25 +0800

    feat-gorse接入浏览和收藏记录

--- models/collect_model.go
+++ models/collect_model.go
@@ -1,5 +1,15 @@
 package models
 
+import (
+	"context"
+	"fast_gin/global"
+	"fmt"
+	"time"
+
+	"github.com/zhenghaoz/gorse/client"
+	"gorm.io/gorm"
+)
+
 type CollectModel struct {
 	Model
 	UserID     uint       `json:"userID"` //用户ID
@@ -7,4 +17,38 @@ type CollectModel struct {
 	GoodsID    uint       `json:"goodsID"`    //商品ID
 	GoodsTitle string     `json:"goodsTitle"` //商品标题
 	GoodsModel GoodsModel `gorm:"foreignKey:GoodsID" json:"-"`
+}
+
+func (c *CollectModel) AfterCreate(tx *gorm.DB) error {
+	if global.Gorse == nil {
+		return nil
+	}
+
+	global.Gorse.InsertFeedback(context.Background(), []client.Feedback{
+		{
+			FeedbackType: "like",
+			UserId:       fmt.Sprintf("%d", c.UserID),
+			ItemId:       fmt.Sprintf("%d", c.GoodsID),
+			Timestamp:    time.Now().Format("2006-01-02 15:04:05"),
+		},
+	})
+
+	return nil
+}
+
+func (c *CollectModel) BeforeDelete(tx *gorm.DB) error {
+	if global.Gorse == nil {
+		return nil
+	}
+
+	global.Gorse.InsertFeedback(context.Background(), []client.Feedback{
+		{
+			FeedbackType: "read",
+			UserId:       fmt.Sprintf("%d", c.UserID),
+			ItemId:       fmt.Sprintf("%d", c.GoodsID),
+			Timestamp:    time.Now().Format("2006-01-02 15:04:05"),
+		},
+	})
+
+	return nil
 }
--- models/look_goods_model.go
+++ models/look_goods_model.go
@@ -1,5 +1,15 @@
 package models
 
+import (
+	"context"
+	"fast_gin/global"
+	"fmt"
+	"time"
+
+	"github.com/zhenghaoz/gorse/client"
+	"gorm.io/gorm"
+)
+
 type LookGoodsModel struct {
 	Model
 	UserID     uint       `json:"userID"` //用户ID
@@ -7,4 +17,21 @@ type LookGoodsModel struct {
 	GoodsID    uint       `json:"goodsID"` //商品ID
 	GoodsTitle string     `json:"goodsTitle"`
 	GoodsModel GoodsModel `gorm:"foreignKey:GoodsID" json:"-"`
+}
+
+func (l *LookGoodsModel) AfterCreate(tx *gorm.DB) error {
+	if global.Gorse == nil {
+		return nil
+	}
+
+	global.Gorse.InsertFeedback(context.Background(), []client.Feedback{
+		{
+			FeedbackType: "read",
+			UserId:       fmt.Sprintf("%d", l.UserID),
+			ItemId:       fmt.Sprintf("%d", l.GoodsID),
+			Timestamp:    time.Now().Format("2006-01-02 15:04:05"),
+		},
+	})
+
+	return nil
 }
