b8626e557b738c2201d70ef871d24f36c36de7c6
NaseemLi <naseem0922@proton.me>
2025-06-03 02:11:22 +0800

    style(routers)-整理路由文件格式，统一风格

--- routers/data_router.go
+++ routers/data_router.go
@@ -10,19 +10,27 @@ import (
 func DataRouter(g *gin.RouterGroup) {
 	app := api.App.DataApi
 
+	// 用户数据相关
 	{
 		g.GET("data/user",
 			middleware.AuthMiddleware,
 			app.StatisticsUserView)
+	}
+
+	// 管理后台数据分析
+	{
 		g.GET("data/system",
 			middleware.AdminMiddleware,
 			app.StatisticsSystemView)
+
 		g.GET("data/user_trend",
 			middleware.AdminMiddleware,
 			app.UserLoginTrendView)
+
 		g.GET("data/order_trend",
 			middleware.AdminMiddleware,
 			app.OrderTrendView)
+
 		g.GET("data/computer",
 			middleware.AdminMiddleware,
 			app.ComputerView)
